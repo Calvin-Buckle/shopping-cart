2360652f18e3cde1a4b3ddcfdfce43f9d6eba298
Calvin Buckle <ccccbuckle@gmail.com>
2024-03-18 16:01:39 +0200

    setup basic clientside router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import Home from "./components/Home"
+import Store from "./components/Store"
+import Cart from "./components/Cart"
 
 function App() {
 
   return (
     <>
-  <Home />
-
+    <div id="heading">
+    <div id="logo"></div>
+    <div id="header">Name comes here</div>
+    <button id="storeButton"><Store />Store</button>
+    <button id="cartButton"><Cart />Cart</button>
+    </div>
+    <p>Body text.....</p>
+    
     </>
-  )
+)
 }
+
 
 export default App
