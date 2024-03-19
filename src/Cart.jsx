79ed64ccdcc93534fa622b501465094e0bce7d6b
Calvin Buckle <ccccbuckle@gmail.com>
2024-03-19 12:42:28 +0200

    create psuedo code for CartItems

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -3,6 +3,11 @@ function Cart(){
     return ( 
         <>
         <Header />
+        <div>
+            <div>Your shopping cart</div>
+            <div>number of items here</div>
+            <div>Array of items here</div>
+        </div>
         </>
     )
 }
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,16 @@
+
+
+function CartItem(){
+    return (
+        <>
+        <div id="cartContainer">
+        <div>item image</div>
+        <div>item name</div>
+        <div>quantity of item</div>
+        <div>price for items</div>
+        <button>Remove</button>
+        </div>
+        
+        </>
+    )
+}
