1a8260282d6b5102dffbba67ce5de0e965d3c6c9
Calvin Buckle <ccccbuckle@gmail.com>
2024-03-19 14:35:22 +0200

    add the api products to cards

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,33 +1,43 @@
-import { useEffect, useState } from "react";
-import { json } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
+function Products() {
+  const [dataArr, setDataArr] = useState([]);
 
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(json => setDataArr(json))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
-function Products(){
-  const [dataArr, setDataArr] = useState([])
-    useEffect(() => {
-        fetch('https://fakestoreapi.com/products')
-        .then(response => response.json())
-        .then(json => setDataArr(json))
-        
-    },[])
-   
-    console.log(dataArr[0])
+  function ProductCard({ dataArr, i }) { 
     return (
-        <>
-        <div className="productContainer">
-        <div className="productImage"></div>
-      
-        <div className="productName"></div>
-        <div className="productPrice"></div>
-        <div className="productRating"></div>
+      <div className="productContainer">
+        <img src={dataArr[i].image} />
+        <div className="productName">{dataArr[i].title}</div>
+        <div className="productPrice">{dataArr[i].price}</div>
+        <div className="productRating">{dataArr[i].rating.rate}</div>
+        <div className="productDescription">{dataArr[i].description}</div>
         <button className="addToCart">Add to Cart</button>
-        </div>
-        
-       
-        </>
-    )
-}
+      </div>
+    );
+  }
 
+  function ProductArr() {
+    const productCards = [];
+    
+    dataArr.forEach((product, index) => {
+      productCards.push(<ProductCard key={index} dataArr={dataArr} i={index} />);
+    });
+
+    return productCards;
+  }
+
+  return (
+    <>
+      <ProductArr />
+    </>
+  );
+}
 
 export default Products;
