edf25f7d5e0569cd7d06b4b8da6957071ad5ba5e
Calvin Buckle <ccccbuckle@gmail.com>
2024-03-26 11:55:12 +0200

    change c of callFilter to C

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,4 +1,4 @@
-const callFilter = ({ isOpen , setIsOpen}) => {
+const CallFilter = ({ isOpen , setIsOpen}) => {
     const handleFilterChange = (e) => {
         console.log(e.target.value)
     };
@@ -21,4 +21,4 @@ const callFilter = ({ isOpen , setIsOpen}) => {
 };
 
 
-export default callFilter;
+export default CallFilter;
