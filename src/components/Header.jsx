4a75c910f47eee1036a2a53fb8e31d9ab4ea33a1
Calvin Buckle <ccccbuckle@gmail.com>
2024-03-27 20:23:30 +0200

    complete styling for cart button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,21 @@
 import { Link } from "react-router-dom";
-import Store from "../Store";
-import Cart from "../Cart";
 
-
-
-function Header(){
-    return (
-        <>
-<div id="heading">
-    <div id="headingsContainer">
-<div id="logo">Fake Api Store</div>
-<div id="header">API Project</div>
-
-<div id="buttonContainer">
-<Link to={'/'}><button className="headerButtons">Home</button></Link>
-<Link to={'/store'}><button className="headerButtons" >Store</button></Link>
-<Link to={'/cart'}><button className="headerButtons">Cart</button></Link>
-</div>
-</div>
-</div>
-
-        </>
-    )
+function Header() {
+  return (
+    <div id="heading">
+      <div id="headingsContainer">
+        <div id="logo">Fake Api Store</div>
+        <div id="header">API Project</div>
+        <div id="buttonContainer">
+          <Link to={'/'}><button className="headerButtons"><p>Home</p></button></Link>
+          <Link to={'/store'}><button className="headerButtons"><p>Store</p></button></Link>
+          <Link to={'/cart'}><button className="headerButtons" id="cartButton" >
+            <div id="totalQuantity">0</div> <p>Cart</p>
+          </button></Link>
+        </div>
+      </div>
+    </div>
+  );
 }
-
 
 export default Header;
--- src/components/UpdateCart.jsx
+++ src/components/UpdateCart.jsx
@@ -3,15 +3,18 @@
 
 let cartArr = [];
 
+
 function UpdateCart({ product, quantity}) {
 
   for(let i = 0; i < quantity; i++){
     cartArr.push(product);
+ 
   console.log(quantity) }
   
    
     return cartArr;
   }
+
 
   export { cartArr };
   export default UpdateCart;
